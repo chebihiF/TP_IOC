6d7110c2d10ce0e0e91075c435a11bdbd91d6cb3
chebihiF <chebihi.f@ucd.ac.ma>
2022-08-22 10:31:22 +0100

    final version

--- src/metier/DataMetier.java
+++ src/metier/DataMetier.java
@@ -2,11 +2,26 @@ package metier;
 
 import dao.*;
 
+import java.io.*;
+
 public class DataMetier {
 
     private IDao dao ;
 
+    private void dao_ioc(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File("ioc.config")));
+            String stringClassName = br.readLine();
+            Class daoClass = Class.forName(stringClassName);
+            dao = (IDao) daoClass.getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
     public double getInfo(){
+        dao_ioc();
         return dao.getData() * 10 ;
     }
 }
